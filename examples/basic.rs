e25f581c23d05faaf1aa5b2cff9bdeb6d014117e
jmcph4 <jack@jmcph4.dev>
2025-03-28 11:28:11 +1000

    Fix basic example

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,9 +1,20 @@
 extern crate xxshim;
 use xxshim::*;
 
-exex!(
-    "My First xxshim ExEx!",
-    async |x| { println!("{x:?}") },
-    async |x, y| { println!("{x:?} {y:?}") },
-    async |x| { println!("{x:?}") }
-);
+pub async fn on_new<N: NodePrimitives>(new: &Blockchain<N>) -> eyre::Result<()> {
+    println!("New block: {}", new.tip().hash());
+    Ok(())
+}
+
+pub async fn on_reorg<N: NodePrimitives>(
+    _old: &Blockchain<N>,
+    _new: &Blockchain<N>,
+) -> eyre::Result<()> {
+    Ok(())
+}
+
+pub async fn on_revert<N: NodePrimitives>(_old: &Blockchain<N>) -> eyre::Result<()> {
+    Ok(())
+}
+
+exex!("My First xxshim!", on_new, on_reorg, on_revert);
